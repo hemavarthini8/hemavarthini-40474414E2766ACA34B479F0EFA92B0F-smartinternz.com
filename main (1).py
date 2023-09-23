1427a51a529419c1ae1ca6f82c97e34805e97efc
hemavarthini8 <144432412+hemavarthini8@users.noreply.github.com>
2023-09-23 14:59:22 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,21 +1,13 @@
-# Python program to check if year is a leap year or not
+def linearsearchproduct(productList, targetproduct):
+  indices= []
+  for index, products in enumerate(productList):
+    if products == targetproduct:
+      indices.append(index)
 
-year = 2000
+  return indices
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
-
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-  print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 == 0) and (year % 100 != 0):
-  print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-  print("{0} is not a leap year".format(year))
+products=["shoes","boot","loafer", "shoes","sandal", "shoes"]
+  
+target ="shoes"  
+result=linearsearchproduct(products,target)
+print(result)
